fb24c9dc4bbb42083024efbc4b83439616167ab3
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2021-04-28 15:39:11 +0200

    wip: db schema

--- main.go
+++ main.go
@@ -10,5 +10,24 @@ func main() {
 		})
 	})
 
+	apiv1 := r.Group("api/v1")
+
+	apiv1.POST("/signup", func(c *gin.Context) {
+		data := c.Request.Body
+		print(data)
+
+		c.JSON(200, gin.H{
+			"_id":            "58457fe6b27...",
+			"email_verified": false,
+			"email":          "test.account@signup.com",
+			"username":       "johndoe",
+			"given_name":     "John",
+			"family_name":    "Doe",
+			"name":           "John Doe",
+			"nickname":       "johnny",
+			"picture":        "http://example.org/jdoe.png",
+		})
+	})
+
 	r.Run()
 }
